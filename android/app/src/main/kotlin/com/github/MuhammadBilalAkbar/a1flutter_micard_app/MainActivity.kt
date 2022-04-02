9014f7e255c7df18d644f743cb4caa8cba9743d3
MuhammadBilalAkbar <mbilalakbarkh786@gmail.com>
2022-04-03 01:31:42 +0500

    Flutter Micard App

---
+++ android/app/src/main/kotlin/com/github/MuhammadBilalAkbar/a1flutter_micard_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.MuhammadBilalAkbar.a1flutter_micard_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              backgroundImage: AssetImage('assets/images/photo.jpg'),
+              radius: 50.0,
+            ),
+            const Text(
+              'Your Name',
+              style: TextStyle(
+                fontSize: 30.0,
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontFamily: 'Source Sans Pro',
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.5,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+              margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+              child: ListTile(
+                leading: const Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'Your cell number.',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.teal.shade900,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: const Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'bilal@email.com',
+                  style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.teal.shade900,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
